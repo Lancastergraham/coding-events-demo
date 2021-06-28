e4eb8360e257257cba12444caca9e47d58f7ed80
Lancaster Graham <Lancastergraham87@gmail.com>
2021-06-28 16:48:21 -0500

    Finished the book part

--- src/main/java/org/launchcode/codingevents/models/Event.java
+++ src/main/java/org/launchcode/codingevents/models/Event.java
@@ -1,13 +1,13 @@
 package org.launchcode.codingevents.models;
 
-import javax.persistence.CascadeType;
-import javax.persistence.Entity;
-import javax.persistence.ManyToOne;
-import javax.persistence.OneToOne;
+import javax.persistence.*;
 import javax.validation.Valid;
+import javax.validation.constraints.Email;
 import javax.validation.constraints.NotBlank;
 import javax.validation.constraints.NotNull;
 import javax.validation.constraints.Size;
+import java.util.ArrayList;
+import java.util.List;
 
 /**
  * Created by Chris Bay
@@ -21,14 +21,18 @@ public class Event extends AbstractEntity {
 
     @OneToOne(cascade = CascadeType.ALL)
     @Valid
-    @NotNull
+    @NotNull(message = "Event details are required")
     private EventDetails eventDetails;
 
     @ManyToOne
     @NotNull(message = "Category is required")
     private EventCategory eventCategory;
 
-    public Event(String name, EventCategory eventCategory) {
+    @ManyToMany
+    private final List<Tag> tags = new ArrayList<>();
+
+    public Event(String name,
+                 EventCategory eventCategory) {
         this.name = name;
         this.eventCategory = eventCategory;
     }
@@ -43,25 +47,38 @@ public class Event extends AbstractEntity {
         this.name = name;
     }
 
-    public EventCategory getEventCategory() {
-        return eventCategory;
-    }
-
-    public void setEventCategory(EventCategory eventCategory) {
-        this.eventCategory = eventCategory;
-    }
-
     public EventDetails getEventDetails() {
         return eventDetails;
     }
 
-    public void setEventDetails(EventDetails eventDetails) {
+    public void setEventDetails(
+            EventDetails eventDetails) {
         this.eventDetails = eventDetails;
+    }
+
+    public EventCategory getEventCategory() {
+        return eventCategory;
+    }
+
+    public void setEventCategory(
+            EventCategory eventCategory) {
+        this.eventCategory = eventCategory;
+    }
+
+    public List<Tag> getTags() {
+        return tags;
+    }
+
+    public void addTag(Tag tag) {
+        this.tags.add(tag);
     }
 
     @Override
     public String toString() {
         return name;
     }
+
+
+
 
 }
